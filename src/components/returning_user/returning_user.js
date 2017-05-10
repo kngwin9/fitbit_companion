851399ee7f1dc4a921ff1d62fc0dc5473dc66a61
Miranda Bashore <miashore@users.noreply.github.com>
2017-05-09 17:55:06 -0700

    commented out for now

--- src/components/returning_user/returning_user.js
+++ src/components/returning_user/returning_user.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import Paper from 'material-ui/Paper';
-import RaisedButton from 'material-ui/RaisedButton';
-import { Link } from 'react-router';
+// import React from 'react';
+// import Paper from 'material-ui/Paper';
+// import RaisedButton from 'material-ui/RaisedButton';
+// import { Link } from 'react-router';
 
-const ReturningUser = () => {
+// const ReturningUser = () => {
 
-    const styles = {
-        paper: {position: 'relative', height: '100%', width: '100%'},
-        h1: {'fontSize': '2em', textAlign: 'center'},
-        p: {width: '75%', margin: '0 auto', textAlign: 'center'},
-        button: {zIndex: 0, width: '75%', position: 'relative', left: '10%', bottom: '5%', margin: '7% 0 0 0'}
-    };
-    return (
-        <Paper style={styles.paper} zDepth={3}>
-            <div style={{padding: '10%'}}>
-                <h1 style={styles.h1}>Welcome Back!</h1>
-                <p style={styles.p}> Based on your activity level from yesterday and your interests we've found events you may like. Come back tomorrow to see new ones!</p>
-                <RaisedButton containerElement={<Link to="/recommended_events"/>} style={styles.button} label="Take Me to These Events!" primary={true} />
-            </div>
-        </Paper>
-    )
-};
+//     const styles = {
+//         paper: {position: 'relative', height: '100%', width: '100%'},
+//         h1: {'fontSize': '2em', textAlign: 'center'},
+//         p: {width: '75%', margin: '0 auto', textAlign: 'center'},
+//         button: {zIndex: 0, width: '75%', position: 'relative', left: '10%', bottom: '5%', margin: '7% 0 0 0'}
+//     };
+//     return (
+//         <Paper style={styles.paper} zDepth={3}>
+//             <div style={{padding: '10%'}}>
+//                 <h1 style={styles.h1}>Welcome Back!</h1>
+//                 <p style={styles.p}> Based on your activity level from yesterday and your interests we've found events you may like. Come back tomorrow to see new ones!</p>
+//                 <RaisedButton containerElement={<Link to="/recommended_events"/>} style={styles.button} label="Take Me to These Events!" primary={true} />
+//             </div>
+//         </Paper>
+//     )
+// };
 
-export default ReturningUser;
+// export default ReturningUser;
